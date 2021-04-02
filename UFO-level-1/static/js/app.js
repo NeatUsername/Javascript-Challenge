b4912b0c3fbe38cec4581b5f9a0dc9a03941c97f
Neat_Username <67394007+NeatUsername@users.noreply.github.com>
2021-04-01 20:34:04 -0400

    interim save

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Step 1:  code to link user click on button to fire our 
+// function (which will populate our dynamic table)
+
+// Here we're using what we learned from activites 3-1 and 3-2.  
+d3.selectAll("#filter-btn").on("click", handleClick);
+
