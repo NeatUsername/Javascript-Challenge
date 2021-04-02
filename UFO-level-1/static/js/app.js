84ce325949dab64fa1e4416f0e5ea5e83115f367
Neat_Username <67394007+NeatUsername@users.noreply.github.com>
2021-04-01 20:55:40 -0400

    interim save

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,9 +3,25 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// Step 1:  code to link user click on button to fire our 
+// Step 1:  Code to link user click on button to fire our 
 // function (which will populate our dynamic table)
 
 // Here we're using what we learned from activites 3-1 and 3-2.  
+
 d3.selectAll("#filter-btn").on("click", handleClick);
 
+// Step 2:  Create the function that will be called by our primary handleClick
+// function to generate the dynamic table.
+
+// We're using insights from activities 3-3
+
+function generateTable(ufo_data) {
+    var tbody = d3.select("tbody");
+
+    // this resets tbody element to null
+    tbody.html("");
+
+    ufo_data.
+
+
+}
